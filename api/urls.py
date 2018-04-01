3e524d4b5d2000ec55100c38d8cebf8eddebbc85
Harshit Jain <hj.harshit007@gmail.com>
2018-04-01 17:07:56 +0530

    Created Admin and User Login APIs

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,7 @@ app_name = 'dbms'
 
 urlpatterns = [
     url(r'^user-register', user_register, name='user-register'),
-    url(r'^admin-register', admin_register, name='admin-register')
+    url(r'^user-login', user_login, name='user-login'),
+    url(r'^admin-register', admin_register, name='admin-register'),
+    url(r'^admin-login', admin_login, name='admin-login')
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime, timedelta
 from .models import *
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from django.contrib.auth.hashers import check_password, make_password
+import jwt
+import os
+import json
 
 
 @api_view(['POST'])
@@ -43,3 +47,51 @@ def admin_register(request):
         Admin.objects.create(username=username, password=hashed_password, first_name=first_name, last_name=last_name)
 
         return Response({"message": "User Successfully Registered"}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def user_login(request):
+    username = request.data['username']
+    password = request.data['password']
+
+    user = User.objects.filter(username=username).first()
+
+    if user:
+        if check_password(password, user.password):
+            jwt_payload = {
+                "username": user.username,
+                "user_type": "user",
+                "exp": datetime.now() + timedelta(days=7)
+            }
+            auth_token = jwt.encode(jwt_payload, os.environ.get('JWT_SECRET'))
+            payload = {
+                "authToken": auth_token
+            }
+
+            return Response(json.dumps(payload), status=status.HTTP_200_OK)
+
+    return Response({"message": "Username or password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def admin_login(request):
+    username = request.data['username']
+    password = request.data['password']
+
+    admin = Admin.objects.filter(username=username).first()
+
+    if admin:
+        if check_password(password, admin.password):
+            jwt_payload = {
+                "username": admin.username,
+                "user_type": "admin",
+                "exp": datetime.now() + timedelta(days=7)
+            }
+            auth_token = jwt.encode(jwt_payload, os.environ.get('JWT_SECRET'))
+            payload = {
+                "authToken": auth_token
+            }
+
+            return Response(json.dumps(payload), status=status.HTTP_200_OK)
+
+    return Response({"message": "Username or password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
